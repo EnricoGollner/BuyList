1979e2ef43cfa1fbaefe28cf99e6dc9b1f07d9d0
Enrico Gollner <enricogollner.tech@gmail.com>
2023-08-09 01:14:35 -0300

    finalizando helper

--- shopping_list/lib/app/data/helper/item_shop_helper.dart
+++ shopping_list/lib/app/data/helper/item_shop_helper.dart
@@ -24,7 +24,7 @@ class ItemShopHelper {
   Future<Database?> initDB() async {
     final databasesPath = await getDatabasesPath();
 
-    final path = "$databasesPath/itemsToShop.db";
+    final String path = "$databasesPath/itemsToShop.db";
 
     return await openDatabase(
       path,
@@ -36,10 +36,88 @@ class ItemShopHelper {
   }
 
   Future<ShopItem> saveShopItem(ShopItem item) async {
-    Database? dbItemShop = await db;
+    Database? dbItemsShop = await db;
 
-    item.id = await dbItemShop?.insert(DBUtils.itemsToShopTable, item.toMap());
-
+    item.id = await dbItemsShop?.insert(DBUtils.itemsToShopTable, item.toMap());
     return item;
+  }
+
+  Future<List<ShopItem>?> getAllItemsToShop() async {
+    Database? dbItemsToShop = await db;
+
+    List<Map<String, dynamic>>? listMap = await dbItemsToShop
+        ?.rawQuery("SELECT * FROM ${DBUtils.itemsToShopTable}");
+
+    if (listMap != null) {
+      List<ShopItem> listItemsToShop = listMap
+          .map(
+            (e) => ShopItem.fromMap(e),
+          )
+          .toList();
+      return listItemsToShop;
+    }
+
+    return null;
+  }
+
+  Future<ShopItem?> getOneItem(int itemToShopId) async {
+    Database? dbItemsToShop = await db;
+
+    List<Map<String, dynamic>>? maps = await dbItemsToShop?.query(
+      DBUtils.itemsToShopTable,
+      columns: [
+        DBUtils.idColumn,
+        DBUtils.isAddedColumn,
+        DBUtils.itemNameColumn,
+      ],
+      where: "${DBUtils.idColumn} = ?",
+      whereArgs: [itemToShopId],
+    );
+
+    if (maps != null && maps.isNotEmpty) {
+      return ShopItem.fromMap(maps.first);
+    }
+
+    return null;
+  }
+
+  Future<int?> deleteById(int itemToShopId) async {
+    Database? dbItemsToShop = await db;
+
+    return await dbItemsToShop?.delete(
+      DBUtils.itemsToShopTable,
+      where: "${DBUtils.idColumn} = ?",
+      whereArgs: [itemToShopId],
+    );
+  }
+
+  Future<int?> updateItem(ShopItem itemToShop) async {
+    Database? dbItemsToShop = await db;
+
+    return await dbItemsToShop?.update(
+      DBUtils.itemsToShopTable,
+      itemToShop.toMap(),
+      where: "${DBUtils.idColumn} = ?",
+      whereArgs: [itemToShop.id],
+    );
+  }
+
+  Future<int?> getNumberOfItemsSaved() async {
+    Database? dbItemsToshop = await db;
+
+    if (dbItemsToshop != null) {
+      return Sqflite.firstIntValue(
+        await dbItemsToshop.rawQuery(
+          "SELECT COUNT(*) FROM ${DBUtils.itemsToShopTable}",
+        ),
+      );
+    }
+
+    return null;
+  }
+
+  Future<void> close() async {
+    Database? dbItemsToshop = await db;
+    dbItemsToshop?.close();
   }
 }
